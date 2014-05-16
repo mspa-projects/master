caee08fec69a61b8bb7602b67ea943f1645936b4
tkrishp <tulasi.krishna.p@gmail.com>
2014-05-15 17:30:27 -0700

    model base code

--- cis_435_travel_main.py
+++ cis_435_travel_main.py
@@ -10,7 +10,7 @@ df_initial = pd.DataFrame(nhts).dropna();
 df_initial['countyid'] = df_initial['tractid'].map(lambda x: str(x)[0:5]);
 
 # Select only relevant features from nhts data frame
-df_nhts = df_initial[['tractid', 'countyid', 'cluster', 'urban_group', 'est_pmiles2007_11', 'est_ptrp2007_11', 'est_vmiles2007_11', 'est_vtrp2007_11', 'median_hh_inc2007_11', 'mean_hh_veh2007_11', 'mean_hh_mem2007_11', 'pct_owner2007_11', 'mean_hh_worker2007_11', 'pct_lchd2007_11', 'pct_lhd12007_11', 'pct_lhd22007_11', 'pct_lhd42007_11']]
+df_nhts = df_initial[['countyid', 'est_pmiles2007_11', 'est_ptrp2007_11', 'est_vmiles2007_11', 'est_vtrp2007_11', 'median_hh_inc2007_11', 'mean_hh_veh2007_11', 'mean_hh_mem2007_11', 'pct_owner2007_11', 'mean_hh_worker2007_11', 'pct_lchd2007_11', 'pct_lhd12007_11', 'pct_lhd22007_11', 'pct_lhd42007_11']]
 
 # Aggregate the nhts dataframe on the countyid and calculate mean() for numeric features
 # This will roll-up the detail nhts file and create one row for each county
